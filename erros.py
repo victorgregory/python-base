34b231541ee684fc84651000275b45a727e55317
Victor Gregory <victor.gregory.cavalheiro@gmail.com>
2022-06-10 04:16:16 -0300

    Erro: EAFP, try, except

--- erros.py
+++ erros.py
@@ -3,18 +3,24 @@
 import sys
 import os
 
-# LBYL - Look Before You Leap
+# EAFP - Easy to ASK Forgiveness than permission
 
-if os.path.exists("names.txt"):
-    print("O arquivo existe")
-    input("...") # Race Condition
-    names = open("names.txt").readlines()
-else:
-    print("[Error] Filenames.txt not found.")
+
+try:
+    names = open("names.txt").readlines() #FileNotFoundError
+
+except FileNotFoundError as e:
+    print(f"{str(e)}.")
     sys.exit(1)
+    # TODO: Usar retry
 
-if len(names) >= 3: 
+else:
+    print("Sucesso!!!")
+finally:
+    print("Execute isso sempre!")
+
+try: 
     print(names[2])
-else: 
+except: 
     print("[Error] Missing name in the list")
     sys.exit(1)
